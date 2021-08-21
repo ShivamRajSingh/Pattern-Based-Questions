c6de76be4e28d4bd41ae7bbf19ca00a8415364bc
Shivam Raj Singh <48837530+shivam3009@users.noreply.github.com>
2021-08-21 21:32:30 +0530

    21-8-21

--- Pattern Tree DFS/Binary_Tree_Path_Sum.cpp
+++ Pattern Tree DFS/Binary_Tree_Path_Sum.cpp
@@ -20,4 +20,14 @@ int main(){
     cout << hasPath(root, sum)? "True": "False";
 
     return 0;
-}
+// }
+// To recursively traverse a binary tree in a DFS fashion, we can start from the root and at every step, make two recursive calls one for the left and one for the right child.
+
+// Here are the steps for our Binary Tree Path Sum problem:
+
+// Start DFS with the root of the tree.
+// If the current node is not a leaf node, do two things:
+// Subtract the value of the current node from the given number to get a new sum => S = S - node.value
+// Make two recursive calls for both the children of the current node with the new number calculated in the previous step.
+// At every step, see if the current node being visited is a leaf node and if its value is equal to the given number ‘S’. If both these conditions are true, we have found the required root-to-leaf path, therefore return true.
+// If the current node is a leaf but its value is not equal to the given number ‘S’, return false.
