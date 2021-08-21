eaa300548a3a20ac869e9a70c9ec8802f118b393
Shivam Raj Singh <48837530+shivam3009@users.noreply.github.com>
2021-08-22 01:02:13 +0530

    22-8-21

---
+++ Pattern Tree DFS/Sum_of_Path_Numbers.cpp
@@ -0,0 +1,34 @@
+#include "codeblock.h"
+
+int helper (TreeNode *root , int sum){
+    if(root == NULL)
+        return 0;
+    
+    sum = 10*sum + root->val;
+
+    if(root->left == NULL && root->right == NULL){
+        return sum;
+    }
+
+    return helper(root->left ,sum)
+        + helper(root->right,sum);
+
+}
+
+
+int sumPath( TreeNode *root){
+    return helper(root, 0);
+}
+
+int main(){
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(7);
+    root->right = new TreeNode(9);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->left = new TreeNode(2);
+    root->right->right = new TreeNode(9);
+    cout << sumPath(root);
+
+    return 0;
+}
