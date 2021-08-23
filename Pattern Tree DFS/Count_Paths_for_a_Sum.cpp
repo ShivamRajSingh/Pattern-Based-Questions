af4bf4e90a78aab9ab33e3f88b95418ea5f6f1ed
Shivam Raj Singh <48837530+shivam3009@users.noreply.github.com>
2021-08-24 01:40:58 +0530

    24-8-21

--- Pattern Tree DFS/Count_Paths_for_a_Sum.cpp
+++ Pattern Tree DFS/Count_Paths_for_a_Sum.cpp
@@ -36,3 +36,20 @@ int main()
     cout << "No. of paths : " << countPaths(root , sum);
     return 0;
 }
+// This problem follows the Binary Tree Path Sum pattern. We can follow the same DFS approach. But there will be four differences:
+
+// We will keep track of the current path in a list which will be passed to every recursive call.
+
+// Whenever we traverse a node we will do two things:
+
+// Add the current node to the current path.
+// As we added a new node to the current path, we should find the sums of all sub-paths ending at the current node. If the sum of any sub-path is equal to ‘S’ we will increment our path count.
+// We will traverse all paths and will not stop processing after finding the first path.
+
+// Remove the current node from the current path before returning from the function. This is needed to Backtrack while we are going up the recursive call stack to process other paths.
+
+
+
+// Time Complexity : Worst Case : O(n^2)
+                    // Best Case : O(nlogn)
+// Space Complexity : O(n)
